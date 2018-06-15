6886e434649a80582d8e641799df51e37cbd9777
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-15 18:46:36 +0000

    fix the comments

--- library_oop.js
+++ library_oop.js
@@ -1,22 +1,3 @@
-/*
-This one will be slightly different from the Week 1 in that it will have the following functionality:
-
-A Library has a name and a creator (both strings)
-These are the only things required in order to create a music library
-A Library has many playlists (starts as an empty array)
-Playlist objects can be added to an instance of a library
-Each Playlist has a name which is required upon creation
-A Playlist also has many tracks
-A Track has a title, a rating (an integer from 1 to 5) and a length (integer in seconds)
- all of which are required when creating a new track
-Each Playlist also has an overallRating function which will calculate the rating by averaging
- the rating of its tracks
-Each Playlist also has a totalDuration function which will sum the duration of all of its tracks
-Once defined, your code should use new to instantiate these functions to create instances of one library
- and then add new tracks and playlists to the library.
-
-*/
-
 class Library {
   constructor(name, creator){
     this.name = name;
